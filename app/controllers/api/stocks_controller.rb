205bc966175a10b85535438d493e4b74e20f2f5c
Deanding <dingtian.ding@gmail.com>
2022-07-27 19:23:08 -0400

    controlelr

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -7,7 +7,6 @@ class Api::StocksController < ApplicationController
       @stock = Stock.new(stock_params)
       @user = User.find_by(id: params[:user_id])
       @stock.user_id = @user.id
-      # need to add logic here for recalculating balance, and see if the transaction is even possible given balance
       total_price = @stock.amount * @stock.unit_price
   
       if (total_price > @user.balance)
